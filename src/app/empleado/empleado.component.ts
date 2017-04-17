ffa7945f8da68521caeb0340395f64942c124dfe
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2017-04-17 11:18:32 -0500

    ok

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,15 +1,50 @@
 import { Component } from '@angular/core';
 
+import  { Empleado } from './empleado'
+
 @Component({
 	selector: 'empleado-tag',
 	templateUrl : './empleado.html',
 
 })
 export class EmpleadoComponent{
-	public nombre_componente = 'Componente de Empleado' ;
-	public list_data = 'Armando Demos xD'
+	public titulo = 'Componente de Empleado' ;
 
-	public nombre:string = "Armando" ;
+	public empleado:Empleado ;
+	public trabajadores:Array<Empleado> ;
+	public trabajador_externo:boolean ;
+
+	constructor(){
+		this.trabajadores = [
+							new Empleado('Armando 1', 31, 'Developer 1', true),
+							new Empleado('Armando 2', 32, 'Developer 2', true),
+							new Empleado('Armando 3', 33, 'Developer 3', true),
+							new Empleado('Armando 4', 34, 'Developer 4', false),
+							new Empleado('Armando 5', 35, 'Developer 5', true),
+							new Empleado('Armando 6', 36, 'Developer 6', true),
+							new Empleado('Armando 7', 37, 'Developer 7', true),
+
+						] ;
+
+
+		this.trabajador_externo =  true ;
+	}
+
+	ngOnInit(){
+		this.empleado = new Empleado('Armando Enrique', 30, 'Developer', true);
+		console.log(this.empleado);
+	}
+
+
+	cambiarExterno(valor){
+		this.trabajador_externo =  valor ;
+
+	}
+
+
+
+
+	/*public nombre:string = "Armando" ;
 	public edad:number = 31 ;
 	public mayorEdad:boolean = true ;
 	public trabajo:Array<any> = ['Developer', 'Fron-End', 'Back-End',44] ;
@@ -34,7 +69,7 @@ export class EmpleadoComponent{
 
 	cambiarEdad(edad){
 		this.edad = edad ;
-	}
+	}*/
 
 
 }
---
+++ src/app/empleado/empleado.ts
@@ -0,0 +1,18 @@
+export class Empleado{
+
+	constructor(
+		public nombre:string,
+		public edad:number,
+		public cargo:string,
+		public contrato:boolean
+	){}
+
+	/*public nombre:string ;
+	public edad: number ;
+
+	constructor(nombre, edad){
+		this.nombre = nombre ;
+		this.edad = edad;
+	}*/
+
+};
